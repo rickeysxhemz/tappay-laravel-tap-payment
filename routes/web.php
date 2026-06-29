1f3388342dda44814bca8ac7dadecf74eea61e20
ghost <rickeysxhemz@gmail.com>
2026-06-29 17:35:42 +0500

    Laravel 13 version compatibility added.fix all legacy code blockers.

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use TapPay\Tap\Http\Controllers\PaymentCallbackController;
 use TapPay\Tap\Http\Controllers\WebhookController;
+use TapPay\Tap\Http\Middleware\VerifyRedirectUrl;
 
-Route::get('callback', PaymentCallbackController::class)->name('callback');
+Route::get('callback', PaymentCallbackController::class)
+    ->middleware(VerifyRedirectUrl::class)
+    ->name('callback');
 
 Route::post('webhook', WebhookController::class)->name('webhook');
